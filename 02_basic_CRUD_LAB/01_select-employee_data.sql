1c3c821945ce036c6027c3e8e9a40dd7f37e1fe8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-19 22:59:56 +0200

    all data on display

--- 02_basic_CRUD_LAB/01_select-employee_data.sql
+++ 02_basic_CRUD_LAB/01_select-employee_data.sql
@@ -5,3 +5,13 @@ FROM employees;
 SELECT job_title AS "Job Title"
 
 FROM employees;
+
+
+// if you want together
+
+SELECT
+	id,
+	first_name || ' ' || last_name AS "Full Name",
+	job_title AS " Job Title"
+
+FROM employees;
